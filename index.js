a7c5dd0e15320c9e0108df2fc633aa30a8c13138
JDJG Inc. Official <ghostzap123@gmail.com>
2021-09-05 19:28:37 +0000

    updated some cool stuff.

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const { Client, Intents } = require("discord.js");
 const token = process.env.TOKEN;
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const allIntents = new Intents(32767);
+const client = new Client({ allIntents });
+
+client.once("ready", () => {
+    console.log("Bot has connected to discord also hi jdjg!");
+});
+
+client.login(token);
