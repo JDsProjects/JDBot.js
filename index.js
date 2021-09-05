a68a89b981b6d04eaa98c78a0e90390534f7c1fe
JDJG Inc. Official <ghostzap123@gmail.com>
2021-09-05 21:55:12 +0000

    ok

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-const { Client, Intents } = require("discord.js"),
-    token = process.env.TOKEN,
-    allIntents = new Intents(32767),
-    client = new Client({ allIntents });
+const { Client, Intents } = require("discord.js");
+
+const client = new Client({ intents: Intents.ALL });
+
+const token = process.env.TOKEN;
 
 client.once("ready", () => {
     client.user.setStatus("online");
+    client.user.setActivity('Playing JDBot but in discord.js', {type: 'PLAYING'});
+
     console.log(`Logged in as ${client.user.tag}`);
     console.log(`Id: ${client.user.id}`);
+
+    let commands = client.application.commands;
+
+    commands.create({
+        name: "hi",
+        description: "says hello"
+    })
+});
+
+client.on("interactionCreate", async interation => {
+    if(!interaction.isCommand()) return;
+
+    const { commandName } = interaction;
+
+    if (commandName = "hi") interaction.reply({});
 });
 
 client.login(token);
