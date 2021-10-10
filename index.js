7472931c837352cfdbcb647040882aa05a05e315
JDJG Inc. Official <ghostzap123@gmail.com>
2021-10-10 23:54:54 +0000

    eh more wip stuff.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const { Client, Intents } = require("discord.js");
 const { SlashCommandBuilder } = require("@discordjs/builders");
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
 const fs = require("fs");
 
 const client = new Client({ intents: 32767 });
@@ -9,19 +11,23 @@ require("http").createServer((req, res) => res.end("JDBot.js is up and running "
 
 let commands = [];
 const files = fs.readdirSync("commands").filter(file => file.endsWith(".js"));
-files.forEach(file => {
-    command = require(`commands${file}`)
-    commands.push(command.data.toJSON())
-});
+files.forEach(file => commands.push(require(`commands${file}`).data.toJSON()));
 
-
-// When the bot is authenticated to Discord
 client.once("ready", () => {
     // Status
     const statuses = [
-        { msg: 'JDBot but in discord.js', type: 'PLAYING' }, 
-        { msg: `${client.guilds.cache.size === 0 ? "No " : client.guilds.cache.size} servers | ${client.users.cache.size === 0 ? "No " : client.users.cache.size} Users`, type: 'WATCHING'},
-        { msg: 'Watching new updates coming soon...', type: 'WATCHING' }
+        { 
+            msg: 'JDBot but in discord.js', 
+            type: 'PLAYING' 
+        }, 
+        {
+            msg: `${client.guilds.cache.size === 0 ? "No " : client.guilds.cache.size} servers | ${client.users.cache.size === 0 ? "No " : client.users.cache.size} Users`,
+            type: 'WATCHING'
+        },
+        {
+            msg: 'Watching new updates coming soon...',
+            type: 'WATCHING'
+        }
     ];
     var i = 0;
     function changeStatus() {
@@ -32,25 +38,41 @@ client.once("ready", () => {
     changeStatus();
     setTimeout(changeStatus, 60000);
 
-    // Debug
     console.log(`Logged in as ${client.user.tag}`);
     console.log(`Id: ${client.user.id}`);
 
-    // Register commands
-    client.application.commands.create({
-        name: "hi",
-        description: "says hello"
-    });
+    // This is a lot of new code I will explain once you get back
+    const rest = new REST({
+        version: '9'
+    }).setToken(process.env.TOKEN);
+    // This is a self executing asyncronous arrow function
+    (async () => {
+        // Here is the error handling you wanted
+        try {
+            await rest.put(
+                // The testing guild is the server that will get the commands without wait
+                Routes.applicationCommands(client.user.id, process.env.TESTING_GUILD), 
+                {
+                    body: commands
+                }
+            );
+        } catch (error) {
+            console.log(error);
+        }
+    })();
 });
 
-// When a command is called
-client.on("interactionCreate", async msg => {
+client.on("interactionCreate", async interaction => {
     // If the command doesn't exist or is being ran by a bot; exit
-    if (!msg.isCommand() || msg.user.bot) return;
-
-    // Next time we will make a modular command handler
-    if (msg.commandName = "hi") await msg.reply({ content: `Hello ${msg.user.username}${msg.user.discriminator}`});
+    if (!interaction.isCommand() || interaction.user.bot) return;
+    const command = commands.get(interaction.commandName);
+    if (!command) return;
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        await interaction.reply({
+            content: "Could't run command due to an error!", ephemeral: true
+        });
+    }
 });
-
-// Authenticate to Discord API
-client.login(process.env.TOKEN);
